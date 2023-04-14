62f5b8d4872940d8a8706a57d5577cbcb234bb61
Maanas Sameer Nandan <maanasnandu@gmail.com>
2023-04-14 14:41:26 -0400

    Add files via upload

---
+++ src/app/components/chart-card/chart-card.component.ts
@@ -0,0 +1,63 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+// @ts-ignore
+import { Chart } from 'chart.js';
+
+//import { BaseChartDirective, Label, Color } from 'ng2-charts';
+
+@Component({
+  selector: 'app-chart-card',
+  templateUrl: './chart-card.component.html',
+  styleUrls: ['./chart-card.component.scss'],
+})
+export class ChartCardComponent implements OnInit {
+  ngOnInit(): void {
+    var myChart = new Chart('myChart', {
+      type: 'bar',
+      data: {
+        labels: ['Health', 'Service', 'HMO', 'POS', 'PPO', 'Platform'],
+        datasets: [
+          {
+            label: '#Platform Monitoring',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
+}
